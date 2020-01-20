b8140df55b7e3a03e5efc88445b2fe01c781162c
someone <someone>
2020-01-19 21:53:13 -0500

    Green. Test cases passes. Nothing to refactor.

---
+++ src/main/java/org/example/CheckoutOrderTotal.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class CheckoutOrderTotal {
+    public void addItemToInventory(String itemName, double itemPrice) {
+    }
+
+    public double getInventoryItemPrice(String itemName) {
+        return 1.0;
+    }
+}
