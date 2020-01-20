08f7ee49b6d0f8b4378a594965909aa112fad93a
someone <someone>
2020-01-19 22:02:12 -0500

    Refactor. Extract the creation of CheckoutOrderTotal outside the test methods. Use a constant instead of repeating a hard-coded number.

--- src/test/java/org/example/CheckoutOrderTotalTest.java
+++ src/test/java/org/example/CheckoutOrderTotalTest.java
@@ -4,18 +4,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CheckoutOrderTotalTest {
+    private static final double PRICE_MAX_DELTA = 0.001;
+    private final CheckoutOrderTotal checkoutOrderTotal = new CheckoutOrderTotal();
+
     @Test
     public void canAddSingleItemToInventory() {
-        CheckoutOrderTotal checkoutOrderTotal = new CheckoutOrderTotal();
-        checkoutOrderTotal.addItemToInventory("Soup", 1.0);
-        Assert.assertEquals(1.0, checkoutOrderTotal.getInventoryItemPrice("Soup"), 0.001);
+        this.checkoutOrderTotal.addItemToInventory("Soup", 1.0);
+        Assert.assertEquals(1.0, this.checkoutOrderTotal.getInventoryItemPrice("Soup"), CheckoutOrderTotalTest.PRICE_MAX_DELTA);
     }
 
     @Test
     public void canAddTwoItemsToInventory() {
-        CheckoutOrderTotal checkoutOrderTotal = new CheckoutOrderTotal();
-        checkoutOrderTotal.addItemToInventory("Soup", 1.0);
-        checkoutOrderTotal.addItemToInventory("Ketchup", 3.0);
-        Assert.assertEquals(3.0, checkoutOrderTotal.getInventoryItemPrice("Ketchup"), 0.001);
+        this.checkoutOrderTotal.addItemToInventory("Soup", 1.0);
+        this.checkoutOrderTotal.addItemToInventory("Ketchup", 3.0);
+        Assert.assertEquals(3.0, this.checkoutOrderTotal.getInventoryItemPrice("Ketchup"), CheckoutOrderTotalTest.PRICE_MAX_DELTA);
     }
 }
