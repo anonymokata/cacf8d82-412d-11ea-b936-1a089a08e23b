0e18872c3bd64294a34a0e7c20e3572a58513a2a
someone <someone>
2020-01-19 21:50:49 -0500

    Red. Started use case #1. Added test to check if single item can be added to the inventory.

--- src/test/java/org/example/CheckoutOrderTotalTest.java
+++ src/test/java/org/example/CheckoutOrderTotalTest.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CheckoutOrderTotalTest {
+
+    @Test
+    public void canAddSingleItemToInventory() {
+        CheckoutOrderTotal checkoutOrderTotal = new CheckoutOrderTotal();
+        checkoutOrderTotal.addItemToInventory("Soup", 1.0);
+        Assert.assertEquals(1.0, checkoutOrderTotal.getInventoryItemPrice("Soup"), 0.001);
+    }
 }
