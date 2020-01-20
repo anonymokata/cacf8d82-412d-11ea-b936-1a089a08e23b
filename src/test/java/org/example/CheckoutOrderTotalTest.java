626e6ce132849a9034049df20cfb6a1bd28476ff
someone <someone>
2020-01-19 21:54:59 -0500

    Red. Added test to check if two items can be added to the inventory.

--- src/test/java/org/example/CheckoutOrderTotalTest.java
+++ src/test/java/org/example/CheckoutOrderTotalTest.java
@@ -11,4 +11,12 @@ public class CheckoutOrderTotalTest {
         checkoutOrderTotal.addItemToInventory("Soup", 1.0);
         Assert.assertEquals(1.0, checkoutOrderTotal.getInventoryItemPrice("Soup"), 0.001);
     }
+
+    @Test
+    public void canAddTwoItemsToInventory() {
+        CheckoutOrderTotal checkoutOrderTotal = new CheckoutOrderTotal();
+        checkoutOrderTotal.addItemToInventory("Soup", 1.0);
+        checkoutOrderTotal.addItemToInventory("Ketchup", 3.0);
+        Assert.assertEquals(3.0, checkoutOrderTotal.getInventoryItemPrice("Ketchup"), 0.001);
+    }
 }
