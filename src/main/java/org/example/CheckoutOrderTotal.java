3630c774d05d0946588ff9ffdd79796bda2e3c99
someone <someone>
2020-01-19 22:12:56 -0500

    Green.

--- src/main/java/org/example/CheckoutOrderTotal.java
+++ src/main/java/org/example/CheckoutOrderTotal.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class CheckoutOrderTotal {
     private HashMap<String, Double> itemNamePriceMap = new HashMap<>();
+    private List<String> scannedItems = new ArrayList<>();
 
     public void addItemToInventory(String itemName, double itemPrice) {
         this.itemNamePriceMap.put(itemName, itemPrice);
@@ -13,10 +16,17 @@ public class CheckoutOrderTotal {
         return this.itemNamePriceMap.get(itemName);
     }
 
-    public void addItem(String soup) {
+    public void addItem(String itemName) {
+        this.scannedItems.add(itemName);
     }
 
     public double computeTotal() {
-        return 1.0;
+        double total = 0.0;
+
+        for (String scannedItem : this.scannedItems) {
+            total += this.itemNamePriceMap.get(scannedItem);
+        }
+
+        return total;
     }
 }
